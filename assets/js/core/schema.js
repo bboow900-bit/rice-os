04e7cdff6ed0d21f8488627bbc1d9c69cce2e73f
Keisuke <bboow900@Gmail.com>
2026-07-28 06:53:55 +0900

    Refresh PWA cache for field management update

--- assets/js/core/schema.js
+++ assets/js/core/schema.js
@@ -5,7 +5,7 @@
   const U = RiceOS.utils;
 
   const SCHEMA_VERSION = 14;
-  const APP_VERSION = "20260727_ver154";
+  const APP_VERSION = "20260728_ver155";
   const STORE_KEY = "rice_os_v8_stable";
   const BACKUP_KEY = "rice_os_v8_stable_backup";
   const LEGACY_STORES = [
